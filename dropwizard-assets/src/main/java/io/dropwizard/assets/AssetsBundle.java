5f51d8e3cf70ce914cd6db81ff9bdc6ab699464f
kerenfi <kerenfi26@gmail.com>
2017-06-06 11:23:23 +0300

    addMethod quality hole

--- dropwizard-assets/src/main/java/io/dropwizard/assets/AssetsBundle.java
+++ dropwizard-assets/src/main/java/io/dropwizard/assets/AssetsBundle.java
@@ -35,6 +35,10 @@ public class AssetsBundle implements Bundle {
     public AssetsBundle() {
         this(DEFAULT_PATH, DEFAULT_PATH, DEFAULT_INDEX_FILE, DEFAULT_ASSETS_NAME);
     }
+    
+    public qualityHole(){
+     return "qh"   
+    }
 
     /**
      * Creates a new AssetsBundle which will configure the application to serve the static files
